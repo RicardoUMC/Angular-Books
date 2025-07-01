10185e102b6b68eaab7fdfd8c8c56ce6690747e1
Ricardo Mora <rmorac1901@alumno.ipn.mx>
2025-07-01 15:58:42 -0600

    Agrega arreglo de libros para pasarlos a BookComponent

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../types/Book';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
+  books: Book[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
